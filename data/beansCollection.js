091ad168879bcae2f208c79bd747a2f2feff9f7f
Bonnie <bonnie.hung@hotmail.ca>
2021-04-11 22:00:46 -0400

    added more beans data

--- data/beansCollection.js
+++ data/beansCollection.js
@@ -1,20 +1,49 @@
 let beansCollection = [
     {
         _id: 1,
-        name: "bean 1",
-        description: "nice beans",
-        size: "500 g",
+        name: "Organic Breakfast Blend",
+        description: 
+            "A mild morning coffee that is smooth, pretty and delicious!  Enjoy the lovely sweet aftertaste of honey and baking spices like nutmeg and biscuit.",
+        size: "350 g",
         price: "15",
         country: "Colombia"
     },
     {
         _id: 2,
-        name: "bean 2",
-        description: "very nice beans",
+        name: "Shadow",
+        description: 
+            "A bold coffee that is rich and complex. It is remarkably smooth and satisfying with a nice solid foundation.  We find subtle notes of almond, walnut and peanut.",
         size: "350 g",
-        price: "20",
-        country: "Uganda"
-    }
+        price: "15",
+        country: "Brazil"
+    },
+    {
+        _id: 3,
+        name: "The Daily",
+        description: 
+            "A medium coffee for everyone. Flavourful and interesting while not being overpowering.   It has dominant flavours of honey and toffee.",
+        size: "350 g",
+        price: "15",
+        country: "Peru"
+    },
+    {
+        _id: 4,
+        name: "Medium Decaf",
+        description: 
+            "A lovely smooth coffee with a sweet aftertaste..",
+        size: "350 g",
+        price: "17",
+        country: "Brazil"
+    },
+    {
+        _id: 5,
+        name: "Commit, an Expresso Blend",
+        description: 
+            "A Bolder coffee – balanced, intense and chocolaty with caramel and nuts. Brilliant as drip, americano, espresso and latte.",
+        size: "350 g",
+        price: "17",
+        country: "Columbia"
+    } 
 ];
 
 module.exports = beansCollection;
